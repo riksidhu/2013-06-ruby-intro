0765de206eeb509c3fd4a240c5b03ff2b80d8a55
Hack Reactor Students <github@hackreactor.com>
2013-07-16 17:03:32 -0700

    queue working

--- ruby_data_structures/src/queue.rb
+++ ruby_data_structures/src/queue.rb
@@ -1,4 +1,23 @@
 class Queue
   def initialize
+    @length = 0
+    @storage = []
   end
+
+  def length
+    @length
+  end
+
+  def add item
+    @storage[@length]=item
+    @length+=1
+  end
+
+  def remove
+    if @length >= 0
+      @length-=1
+      @storage.shift
+    end
+  end
+
 end
